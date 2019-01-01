9640f73ab4b2e8e883c0fda9b4a292c167237f25
unknown <2010112122@dongguk.edu>
2019-01-01 18:49:38 +0900

    파일 다운로드까지 됨, 디렉토리일 경우 안됨

--- routes/file.js
+++ routes/file.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var targetDir = './';
+var targetDir = './files/';
 var fs = require('fs');
 
 /* 파일 */
 router.get('/', function(req, res, next) {
     //res.send('hello');
   fs.readdir(targetDir, function(error, filelist){
-    console.log(filelist);
-      res.send(filelist);
+    res.render('filelist', { files: filelist });
+    //console.log(filelist);
+      //res.send(filelist);
       
   });
 });
-
+router.get('/:filename', async (req, res) => {
+    var file = fs.readFileSync(targetDir + req.params.filename, 'binary');
+    res.setHeader('Content-Length', file.length);
+    res.write(file, 'binary');
+    res.end();
+});
 module.exports = router;
