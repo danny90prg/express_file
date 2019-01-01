93cf54a51ccbc5e10b032d91f522ea6e3f766932
unknown <2010112122@dongguk.edu>
2019-01-01 12:09:17 +0900

    filerouter 연결하였으며, 파일목록 반환하도록 코드 동작 확인함

--- routes/file.js
+++ routes/file.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var targetDir = './';
+var fs = require('fs');
 
 /* 파일 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    //res.send('hello');
+  fs.readdir(targetDir, function(error, filelist){
+    console.log(filelist);
+      res.send(filelist);
+      
+  });
 });
 
 module.exports = router;
